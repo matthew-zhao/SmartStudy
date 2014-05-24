a9d8e457b474fbdda438e0e32b9689d6e6b1afd4
Irving Chen <irvingc@hotmail.com>
2014-05-24 16:16:01 -0700

    new main menu layout

--- app/gen/com/dongsquad/smartstudy/R.java
+++ app/gen/com/dongsquad/smartstudy/R.java
@@ -24,11 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int button1=0x7f080000;
-        public static final int button2=0x7f080001;
-        public static final int button3=0x7f080002;
-        public static final int button4=0x7f080003;
+        public static final int action_settings=0x7f080006;
+        public static final int btn_main_edit=0x7f080001;
+        public static final int btn_main_import=0x7f080002;
+        public static final int btn_main_new_quiz=0x7f080000;
+        public static final int btn_main_quit=0x7f080005;
+        public static final int btn_main_see_results=0x7f080004;
+        public static final int btn_main_take_quiz=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -40,10 +42,12 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
-        public static final int main_menu_btn_edit_quiz=0x7f050004;
+        public static final int main_menu_btn_edit=0x7f050004;
+        public static final int main_menu_btn_import=0x7f050005;
         public static final int main_menu_btn_new_quiz=0x7f050003;
-        public static final int main_menu_btn_see_results=0x7f050006;
-        public static final int main_menu_btn_take_quiz=0x7f050005;
+        public static final int main_menu_btn_quit=0x7f050008;
+        public static final int main_menu_btn_see_results=0x7f050007;
+        public static final int main_menu_btn_take_quiz=0x7f050006;
     }
     public static final class style {
         /** 
---
+++ app/src/com/dongsquad/smartstudy/Term.java
@@ -0,0 +1,13 @@
+package com.dongsquad.smartstudy;
+
+public class Term {
+
+	public final String term;
+	public final String definition;
+	
+	public Term(String term, String definition) {
+		this.term = term;
+		this.definition = definition;
+	}
+	
+}
