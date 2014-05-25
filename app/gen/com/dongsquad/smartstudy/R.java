b70fcfde78b8928e41f0e9144c3d2e5d5121f784
Irving Chen <irvingc@hotmail.com>
2014-05-25 00:48:53 -0700

    organized strings

--- app/gen/com/dongsquad/smartstudy/R.java
+++ app/gen/com/dongsquad/smartstudy/R.java
@@ -24,26 +24,40 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
-        public static final int btn_main_edit=0x7f080001;
-        public static final int btn_main_import=0x7f080002;
-        public static final int btn_main_new_quiz=0x7f080000;
-        public static final int btn_main_quit=0x7f080005;
-        public static final int btn_main_see_results=0x7f080004;
-        public static final int btn_main_take_quiz=0x7f080003;
-        public static final int btn_term_categories=0x7f08000b;
-        public static final int btn_term_exit=0x7f08000a;
-        public static final int content_layout=0x7f080006;
-        public static final int lbl_number=0x7f080007;
-        public static final int txt_term_definition=0x7f080009;
-        public static final int txt_term_name=0x7f080008;
+        public static final int action_settings=0x7f080018;
+        public static final int btn_main_edit=0x7f080008;
+        public static final int btn_main_import=0x7f080009;
+        public static final int btn_main_new_quiz=0x7f080007;
+        public static final int btn_main_quit=0x7f08000c;
+        public static final int btn_main_see_results=0x7f08000b;
+        public static final int btn_main_take_quiz=0x7f08000a;
+        public static final int btn_term_categories=0x7f080013;
+        public static final int btn_term_exit=0x7f080012;
+        public static final int button1=0x7f080004;
+        public static final int button2=0x7f080005;
+        public static final int button3=0x7f08000d;
+        public static final int checkbox=0x7f080015;
+        public static final int content_layout=0x7f08000e;
+        public static final int editText1=0x7f080002;
+        public static final int lbl_category_name=0x7f080014;
+        public static final int lbl_dialog_title=0x7f080016;
+        public static final int lbl_number=0x7f08000f;
+        public static final int linearLayout1=0x7f080006;
+        public static final int listView1=0x7f080017;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080001;
+        public static final int textView3=0x7f080003;
+        public static final int txt_term_definition=0x7f080011;
+        public static final int txt_term_name=0x7f080010;
     }
     public static final class layout {
         public static final int activity_import=0x7f030000;
-        public static final int activity_learning=0x7f030001;
+        public static final int activity_know=0x7f030001;
         public static final int activity_main=0x7f030002;
         public static final int activity_results=0x7f030003;
         public static final int activity_term=0x7f030004;
+        public static final int category_list_item=0x7f030005;
+        public static final int fragment_categories_dialog=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -53,6 +67,9 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int import_btn_add=0x7f050013;
+        public static final int import_btn_exit=0x7f050014;
+        public static final int know_show_answer=0x7f050011;
         public static final int main_menu_btn_edit=0x7f050004;
         public static final int main_menu_btn_import=0x7f050005;
         /**  MainActivity 
@@ -61,11 +78,21 @@ public final class R {
         public static final int main_menu_btn_quit=0x7f050008;
         public static final int main_menu_btn_see_results=0x7f050007;
         public static final int main_menu_btn_take_quiz=0x7f050006;
+        public static final int results_btn_correct=0x7f050016;
+        public static final int results_btn_perquestion=0x7f050017;
+        /**  ResultsActivity 
+         */
+        public static final int results_btn_time=0x7f050015;
         public static final int results_time=0x7f05000e;
         public static final int term_btn_categories=0x7f05000d;
         public static final int term_btn_exit=0x7f05000c;
         public static final int term_hint_definition=0x7f05000b;
         public static final int term_hint_term=0x7f05000a;
+        /**  ImportActivity 
+         */
+        public static final int title_activity_import=0x7f050012;
+        public static final int title_activity_know=0x7f05000f;
+        public static final int title_activity_results=0x7f050010;
         /**  TermActivity 
          */
         public static final int title_activity_term=0x7f050009;
