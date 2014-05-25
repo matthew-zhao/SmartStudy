01f2f7131a75df1dbebab34c602c38d4ff22d56b
Matthew Zhao <matthew.x.zhao@gmail.com>
2014-05-25 07:28:04 -0700

    Forgot to change to label2

--- app/src/com/dongsquad/smartstudy/ResultsActivity.java
+++ app/src/com/dongsquad/smartstudy/ResultsActivity.java
@@ -13,6 +13,9 @@ import android.widget.TextView;
 public class ResultsActivity extends Activity {
 	private TableLayout table;
 	private int index = 0;
+	private ServiceArgs loginArgs = new ServiceArgs();
+	private Service service;
+	private TermSet set;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -35,12 +38,41 @@ public class ResultsActivity extends Activity {
 			row.addView(label);
 			
 			TextView label2 = new TextView(this);
-			label.setText(LearnActivity.times.get(i) + " milliseconds");
+			label2.setText(LearnActivity.times.get(i) + " milliseconds");
 			row.addView(label2);
 		}
 	}
 	public void getCorrect(View source)
 	{
-		
+		/**loginArgs.setUsername("admin");
+		loginArgs.setPassword("codeday2014");
+		loginArgs.setHost("localhost");
+		loginArgs.setPort(8089);
+		service = Service.connect(loginArgs);
+		int size = set.getCategories().size();
+		Character[] charat = set.getCategories().toArray(null);
+		for (int i = 0; i < size; i++)
+		{
+			TableRow row = new TableRow(this);
+			TextView label = new TextView(this);
+			
+			label.setText(charat[i]);
+			row.addView(label);
+			
+			TextView label2 = new TextView(this);
+			label2
+		}
+		/**for (int i = 0; i < set.getTerms().size(); i++ )
+		{
+			TableRow row = new TableRow(this);
+			TextView label = new TextView(this);
+			
+			int numCategories = set.getTerms().get(i).getCategories().size();
+			for (int j = 0; i < max; i++)
+			{	
+			String category = set.getTerms().get(i).getCategories().;
+			label.setText("");
+			
+		}*/
 	}
 }
